da9e7f0cf4609444c421a304245b9b34f3c8607f
Pronin Aleksandr <makkurano@gmail.com>
2019-05-20 00:05:10 +0300

    Done!

--- app/src/main/java/com/example/proninhw213/MainActivity.java
+++ app/src/main/java/com/example/proninhw213/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
-
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
